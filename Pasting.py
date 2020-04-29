467bad6ece12d17dae87a52e30f5f989e508c3de
Dareten <30283450+Dareten@users.noreply.github.com>
2020-04-29 15:52:13 +0300

    Update Pasting.py
    Чтоб не гуглить

--- Pasting.py
+++ Pasting.py
@@ -1,13 +1,13 @@
-import keyboard
-from tkinter import Tk
+import keyboard #библиотека для управления клавиатурой, в частности установка хоткеев
+from tkinter import Tk # библиотека, которая здесь получает значение буфера обмена в переменную
 
 def foo():
-    c = Tk()
-    c.withdraw()
-    clip = c.clipboard_get()
+    c = Tk() # инициалиция объекта
+    c.withdraw() # удаление окна библиотеки
+    clip = c.clipboard_get() # получение данных
     c.update()
-    c.destroy()
-    keyboard.write(clip)
+    c.destroy() # обновление и удаление объекта
+    keyboard.write(clip) # печать переменной
 
-keyboard.add_hotkey('ctrl+v', foo)
-keyboard.wait('ctrl+q')
+keyboard.add_hotkey('ctrl+v', foo) # добавление хоткея
+keyboard.wait('ctrl+q') # добавление хоткея на завершение прогаммы
