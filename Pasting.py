17c03d21890d92b160ae160667f516ee97682a63
Dareten <30283450+Dareten@users.noreply.github.com>
2020-05-27 16:30:57 +0300

    Все робит же

--- Pasting.py
+++ Pasting.py
@@ -1,17 +1,17 @@
 import keyboard
 import win32clipboard
 from time import sleep
+from random import randint
 
 def foo():
     sleep(1)
     win32clipboard.OpenClipboard()
     data = win32clipboard.GetClipboardData().replace('\n', '').replace('\t', '')
     win32clipboard.CloseClipboard()
-    keyboard.write(data, delay=0.01)
+    keyboard.write(data, delay=randint(10,50)/1000)
     keyboard.send('ctrl+a')
     keyboard.send('tab')
     keyboard.send('shift+tab')
-
 
 
 keyboard.add_hotkey('ctrl+v', foo)
